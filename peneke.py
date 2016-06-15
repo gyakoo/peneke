2a84dfd212ddaa4cd84066a62d4bd806ee9e75a5
Manu <gyakoo@gmail.com>
2016-06-14 21:32:37 -0700

    new scene class

--- peneke.py
+++ peneke.py
@@ -8,17 +8,25 @@ import engine
 import pytmx
 from pytmx.util_pygame import *
 
+
+class Scene(engine.Actor):
+    def __init__(self,engine):
+        super(Scene,self).__init__(engine)
+        self.tile_map = load_pygame("data/test01.tmx")
+    
+    def draw(self):
+        surf = self.tile_map.get_tile_image(0,0,0)
+
 # --------------------------------------------------------
 class Player(engine.Actor):
     def __init__(self,engine):
         super(Player,self).__init__(engine)
-        self.tile_map = load_pygame("data/test01.tmx")
 
     def mouseUp(self,pos):
         None
 
     def draw(self):
-        surf = self.tile_map.get_tile_image(0,0,0)
+        None
 
 
 # --------------------------------------------------------
@@ -28,8 +36,8 @@ if __name__ == '__main__':
     # Initialize engine and actors
     pygame.init()
     eng = engine.Engine( "Peneke", (640,480) )
-    player = Player(eng)    
-    eng.addActor( player )
+    eng.addActor( Player(eng) )
+    eng.addActor( Scene(eng) )
     #pygame.mouse.set_visible(0)
 
     # Main loop
